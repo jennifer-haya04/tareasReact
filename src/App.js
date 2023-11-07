5c36e9a733427773bb48094620decb5f1352aafa
unknown <jenha04@gmail.com>
2023-11-07 14:47:37 -0500

    Tareas V:Final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './imagenes/FreeCodeCamp_logo.png'
+import ListaTareas from './componentes/ListaTareas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='aplicacion-tareas'>
+      <div className='logo-contenedor'>
+        <img src={logo} className='logo' />
+      </div>
+      <div className='tareas-principal'>
+        <h1>Mis tareas</h1>
+        <ListaTareas />
+      </div>
     </div>
   );
 }
